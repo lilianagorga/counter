2392affdfd61a589629e2a6fdaaa69782ddf69da
Liliana Gorga <gorgaliliana87@gmail.com>
2023-06-04 10:56:22 +0200

    add btn event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let count = 0;
+
+const value = document.getElementById('value');
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    const styles = e.currentTarget.classList;
+      if(styles.contains('decrease')) {
+        count--;
+      } else if(styles.contains('increase')) {
+        count++;
+      } else {
+        count = 0;
+        }
+      if(count > 0) {
+        value.style.color = '#009432';
+      }
+      if(count < 0) {
+        value.style.color = '#ea2027';
+      }
+      if(count === 0) {
+        value.style.color = '#262626';
+      }
+      value.textContent = count;
+    });
+});
